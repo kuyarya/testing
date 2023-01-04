3ca68650283f1b7bb6d4e0b0d136074bf5050e8f
ariopri <arioprima1@gmail.com>
2023-01-04 23:04:36 +0700

    feat: create user entity

---
+++ backend/entities/user.go
@@ -0,0 +1,11 @@
+package entities
+
+type User struct {
+	ID        int64  `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
+}
