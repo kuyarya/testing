71510914daaa6b227d56eba195fa10accef2a385
ariopri <arioprima1@gmail.com>
2023-01-04 23:06:02 +0700

    feat: create build backend configuration in main

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/ariopri/Let-It-Be/tree/main/backend/config"
+	"github.com/ariopri/Let-It-Be/tree/main/backend/entities"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.Use(config.CORS(config.DefaultCORSConfig()))
+	r.GET("/informasi", func(c *gin.Context) {
+		c.JSON(http.StatusOK, entities.InformasiData)
+	})
+	r.Run()
 }
