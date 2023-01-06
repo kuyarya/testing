1ca130389b652c1585a2133c632b616a7cd3a71b
ariopri <arioprima1@gmail.com>
2023-01-06 19:28:43 +0700

    feat : setup login and register handler in main

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/ariopri/Let-It-Be/tree/main/backend/config"
+	"github.com/ariopri/Let-It-Be/tree/main/backend/database/migration"
 	"github.com/ariopri/Let-It-Be/tree/main/backend/entities"
+	"github.com/ariopri/Let-It-Be/tree/main/backend/handler"
+	"github.com/ariopri/Let-It-Be/tree/main/backend/repository"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,5 +18,21 @@ func main() {
 	r.GET("/informasi", func(c *gin.Context) {
 		c.JSON(http.StatusOK, entities.InformasiData)
 	})
+
+	//initial databased
+	db, err := sql.Open("mysql", "root:tanahdamai@tcp(localhost:3306)/letitbe")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//Run Migration
+	migration.Migrate(db)
+
+	//Initial Repository
+	siswaRepo := repository.NewSiswaRepository(db)
+
+	//Initial Handler
+	handler.NewSiswaHandler(r, siswaRepo)
 	r.Run()
 }
